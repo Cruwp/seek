d949de17a103923b96139f4d30caf88b8cbf59b0
hugo-clems <hugo.clems@outlook.fr>
2016-03-19 14:54:38 +0100

    Encodage UTF-8

--- inc/bdd.php
+++ inc/bdd.php
@@ -6,6 +6,9 @@
         $pdo = null;
         try {
             $pdo = new PDO("mysql:host=localhost;dbname=bd_seek", "root", "");
+
+            // Passe en utf-8
+			$pdo->query("SET NAMES utf8");
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
