d873f1650719935cf314adc8844196d3e510589b
romaincauffet <romain.cauffet@gmail.com>
2016-05-19 20:54:31 +0200

    je test ca race

--- deconnexion.php
+++ deconnexion.php
@@ -1,6 +1,1 @@
-<?php
-    session_start(); // permet d'utiliser les sessions
-    // on casse la session => user plus connecté
-    session_destroy();
-    // redirection
-    header("location:index");
+NIQUE TA RACE
