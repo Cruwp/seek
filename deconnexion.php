cf99cd6a48e3463c69cced1d3812def7f1a082d4
Guillaume <G.Cruwp@gmail.com>
2016-05-19 20:56:04 +0200

    Je répare les bêtises

--- deconnexion.php
+++ deconnexion.php
@@ -1,1 +1,6 @@
-NIQUE TA RACE
+<?php
+    session_start(); // permet d'utiliser les sessions
+    // on casse la session => user plus connecté
+    session_destroy();
+    // redirection
+    header("location:index");
